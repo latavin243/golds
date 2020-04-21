e69eae9907f032c44d332a7fbca8dce123528026
monitor1379 <yy4f5da2@hotmail.com>
2020-04-21 23:43:47 +0800

    MOD

--- client.go
+++ client.go
@@ -9,7 +9,7 @@ import (
  * @Author: ZhenpengDeng(monitor1379)
  * @Date: 2020-04-21 22:39:22
  * @Last Modified by: ZhenpengDeng(monitor1379)
- * @Last Modified time: 2020-04-21 22:51:33
+ * @Last Modified time: 2020-04-21 23:11:40
  */
 
 const (
@@ -17,8 +17,10 @@ const (
 )
 
 type Client struct {
-	conn   net.Conn
-	locker sync.Mutex
+	conn          net.Conn
+	locker        *sync.Mutex
+	packetEncoder *PacketEncoder
+	packetDecoder *PacketDecoder
 }
 
 func Dial(address string) (*Client, error) {
@@ -26,17 +28,34 @@ func Dial(address string) (*Client, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	client := &Client{
-		conn:   conn,
-		locker: sync.Mutex{},
+		conn:          conn,
+		locker:        &sync.Mutex{},
+		packetEncoder: NewPacketEncoder(conn),
+		packetDecoder: NewPacketDecoder(conn),
 	}
 	return client, nil
 }
 
 func (this *Client) Set(key, value []byte) error {
+	packet := &Packet{
+		PacketType: PacketTypeString,
+		Value:      []byte("fuck you"),
+	}
+	_, err := this.packetEncoder.Encode(packet)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (this *Client) Get(key []byte) ([]byte, error) {
-	return nil, nil
+	packet, err := this.packetDecoder.Decode()
+	if err != nil {
+		return nil, nil
+	}
+
+	// TODO(monitor1379): 解析packet.Value或者key对应的value
+	return packet.Value, nil
 }
